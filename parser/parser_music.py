faace229489762cfd347ae80b5e77d804e271e03
Danniel <13411054963@163.com>
2019-03-31 16:20:12 +0800

    init commit

---
+++ parser/parser_music.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: 10858
+@license: (C) Copyright
+@contact: xxx@qq.com
+@software: PyCharm
+@file: parser_music.py
+@time: 2019-03-31 16:16
+@desc:
+'''
