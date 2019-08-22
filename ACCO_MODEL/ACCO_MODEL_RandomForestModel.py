1748e0a0f0972fffc32a93c2ae1548077328ed6a
dannieldwt <1085837135@qq.com>
2019-08-22 22:44:26 +0800

    增加几种待测试模型
    随机森林与SVM

---
+++ ACCO_MODEL/ACCO_MODEL_RandomForestModel.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: 10858
+@license: (C) Copyright
+@contact: xxx@qq.com
+@software: PyCharm
+@file: ACCO_MODEL_RandomForestModel.py
+@time: 2019-08-22 22:22
+@desc:
+'''
+
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.preprocessing import StandardScaler
+
+class randomForstModel:
+    '''
+    建立随机森林模型，用于训练和预测
+    '''
+
+    def __init__(self):
+        self.__model = RandomForestClassifier()
+        #self.__ss = StandardScaler()
+
+    def train(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=33)
+        self.__model.fit(X_train, y_train)
+        loss = self.__model.score(X_test, y_test)
+        #predict_y = self.__model.predict(X_test)
+        #mae = mean_absolute_error(predict_y, y_test)
+        #mse = mean_squared_error(predict_y, y_test)
+        return loss
+
+    def predict(self, X):
+        predict_y = self.__model.predict(X)
+        return np.mat(predict_y)
---
+++ ACCO_MODEL/ACCO_MODEL_SVMModel.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: 10858
+@license: (C) Copyright
+@contact: xxx@qq.com
+@software: PyCharm
+@file: ACCO_MODEL_SVMModel.py
+@time: 2019-08-22 22:37
+@desc:
+'''
+
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
+class randomForstModel:
+    '''
+    建立随机森林模型，用于训练和预测
+    '''
+
+    def __init__(self):
+        self.__model = SVC(kernel='rbf',random_state=33,gamma=0.10,C=10.0)
+        #self.__ss = StandardScaler()
+
+    def train(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=33)
+        self.__model.fit(X_train, y_train)
+        loss = self.__model.score(X_test, y_test)
+        #predict_y = self.__model.predict(X_test)
+        #mae = mean_absolute_error(predict_y, y_test)
+        #mse = mean_squared_error(predict_y, y_test)
+        return loss
+
+    def predict(self, X):
+        predict_y = self.__model.predict(X)
+        return np.mat(predict_y)
+
